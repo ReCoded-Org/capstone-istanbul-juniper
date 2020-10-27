630a1a4a6d06289e8a33ec6b152bc2a795baedb7
Majd Ajroudi <majd.ajroudi@gmail.com>
2020-10-27 23:26:09 +0100

    added navbar component and its test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import Home from "./containers/Home/index"
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import 'antd/dist/antd.css'
+import {useTranslation} from "react-i18next"
 
 function App() {
-  return <div>Basic structure</div>;
+  const {i18n} = useTranslation()
+  document.body.dir = i18n.dir()
+
+  return (
+    <>
+      <Router>
+        <Route exact path="/" component={Home}/> 
+        <Route path="/about" component={""} />
+        <Route path="/games" component={""} />
+        <Route path="/resources" component={""} />
+        <Route path="/contact" component={""} />
+        <Route path="/login" component={""} />
+
+      </Router>  
+    </>
+    );
 }
 
 export default App;
---
+++ src/containers/Home/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../../components/Navbar/index"
+import "./index.css"
+import {} from "antd"
+
+
+const Home = () => {
+    return(
+        <>
+            <Navbar /> 
+        </>
+    )
+}
+
+export default Home
