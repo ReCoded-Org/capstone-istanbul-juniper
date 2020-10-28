e74246b7a0e8e9d9f701c720ea67e06005d29abc
Majd Ajroudi <majd.ajroudi@gmail.com>
2020-10-28 17:41:21 +0100

    remove all commented code

--- src/components/Navbar/index.test.js
+++ src/components/Navbar/index.test.js
@@ -2,7 +2,6 @@ import React from "react";
 import Navbar from "./index.jsx";
 import renderer from "react-test-renderer";
 
-// const renderer = require('react-test-renderer');
 describe("Navbar Component", () => {
   it("matches the tree", () => {
     const tree = renderer.create(<Navbar />).toJSON();
