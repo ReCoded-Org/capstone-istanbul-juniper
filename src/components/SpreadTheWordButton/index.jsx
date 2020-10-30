8e7bf9db3c40cfc64eb2cd6e698a4ad22a80f295
Tarkan Mansuroğlu <67609059+tmansuroglu@users.noreply.github.com>
2020-10-30 13:57:40 +0300

    Spread the word button (#14)

---
+++ src/components/SpreadTheWordButton/index.jsx
@@ -0,0 +1,33 @@
+/*
+ * Functionality of this button is not %100 agreed upon.
+ * So, the link it directs to is not correct.
+ */
+
+import React from "react";
+import { Row, Col } from "antd";
+import { TwitterCircleFilled } from "@ant-design/icons";
+import "./index.css";
+import { useTranslation } from "react-i18next";
+
+const SpreadTheWordButton = () => {
+  const [t] = useTranslation();
+
+  return (
+    <Row justify="center" className="spreadTheWord">
+      <Col xxl={10} xl={10} lg={12} md={12} sm={14} xs={16}>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.twitter.com"
+        >
+          <button type="button" className="spreadTheWord__button">
+            <TwitterCircleFilled className="twitterIcon" />
+            {t("home.spreadTheWord")}
+          </button>
+        </a>
+      </Col>
+    </Row>
+  );
+};
+
+export default SpreadTheWordButton;
--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import BottomCurve from "../../components/BottomCurve/index";
+import SpreadTheWordButton from "../../components/SpreadTheWordButton";
 import "./index.css";
 
 const Home = () => {
   return (
     <>
       <BottomCurve />
+      <SpreadTheWordButton />
     </>
   );
 };
