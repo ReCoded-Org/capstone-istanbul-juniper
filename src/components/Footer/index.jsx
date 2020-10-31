d8582e46484aee0e7c9fb43367b3a27a58115db0
YamanSammani <yaman.mys@gmail.com>
2020-10-31 12:53:29 +0000

    updating Footer className, removing extra components

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -14,52 +14,61 @@ const Footer = () => {
   const [t] = useTranslation();
 
   return (
-    <footer>
-      <Router>
-        <img
-          alt="globe"
-          src="/assets/images/earthNav.svg"
-          className="footerGlobe"
-        />
-        <Col>
-          <Row gutter={[25]} justify="center" className="footerLinksContainer">
-            <Col className="footerLinksContainer__nav">
-              <Link to="/trademark"> {t("footer.trademark")}</Link>
-            </Col>
-            <Col className="footerLinksContainer__nav">
-              <Link to="/contact"> {t("footer.contact")}</Link>
-            </Col>
-            <Col className="footerLinksContainer__nav">
-              <Link to="/policies"> {t("footer.policies")}</Link>
-            </Col>
-            <Col className="footerLinksContainer__nav">
-              <Link to="/games"> {t("footer.games")}</Link>
-            </Col>
-            <Col className="footerLinksContainer__nav">
-              <Link to="/about"> {t("footer.about")}</Link>
-            </Col>
-          </Row>
-          <Row justify="center" className="footerSocialContainer">
-            <Col>
-              {/* note: facebook,instagram,youtube are built in fortawesome classes */}
-              <a href="https://www.facebook.com" className="facebook social">
-                <FontAwesomeIcon icon={faFacebook} size="2x" />
-              </a>
-            </Col>
-            <Col>
-              <a href="https://www.instagram.com" className="instagram social">
-                <FontAwesomeIcon icon={faInstagram} size="2x" />
-              </a>
-            </Col>
-            <Col>
-              <a href="https://www.youtube.com" className="youtube social">
-                <FontAwesomeIcon icon={faYoutube} size="2x" />
-              </a>
-            </Col>
-          </Row>
-        </Col>
-      </Router>
-    </footer>
+    <div className="footerContainer">
+      <footer>
+        <Router>
+          <img
+            alt="globe"
+            src="/assets/images/earthNav.svg"
+            className="footerGlobe"
+          />
+          <Col>
+            <Row
+              gutter={[25]}
+              justify="center"
+              className="footerLinksContainer"
+            >
+              <Col className="footerLinksContainer__nav">
+                <Link to="/trademark"> {t("footer.trademark")}</Link>
+              </Col>
+              <Col className="footerLinksContainer__nav">
+                <Link to="/contact"> {t("footer.contact")}</Link>
+              </Col>
+              <Col className="footerLinksContainer__nav">
+                <Link to="/policies"> {t("footer.policies")}</Link>
+              </Col>
+              <Col className="footerLinksContainer__nav">
+                <Link to="/games"> {t("footer.games")}</Link>
+              </Col>
+              <Col className="footerLinksContainer__nav">
+                <Link to="/about"> {t("footer.about")}</Link>
+              </Col>
+            </Row>
+            <Row justify="center" className="footerSocialContainer">
+              <Col>
+                {/* note: facebook,instagram,youtube are built in fortawesome classes */}
+                <a href="https://www.facebook.com" className="facebook social">
+                  <FontAwesomeIcon icon={faFacebook} size="2x" />
+                </a>
+              </Col>
+              <Col>
+                <a
+                  href="https://www.instagram.com"
+                  className="instagram social"
+                >
+                  <FontAwesomeIcon icon={faInstagram} size="2x" />
+                </a>
+              </Col>
+              <Col>
+                <a href="https://www.youtube.com" className="youtube social">
+                  <FontAwesomeIcon icon={faYoutube} size="2x" />
+                </a>
+              </Col>
+            </Row>
+          </Col>
+        </Router>
+      </footer>
+    </div>
   );
 };
 
--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,13 +1,8 @@
 import React from "react";
-import BottomCurve from "../../components/BottomCurve/index";
 import "./index.css";
 
 const Home = () => {
-  return (
-    <>
-      <BottomCurve />
-    </>
-  );
+  return <></>;
 };
 
 export default Home;
