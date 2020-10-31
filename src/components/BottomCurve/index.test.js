94bf2f46ddbed158c7ea08909ad45b10ec466ea5
YamanSammani <yaman.mys@gmail.com>
2020-10-31 12:26:13 +0000

    removing extra components

--- src/components/BottomCurve/index.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import BottomCurve from ".";
-
-it("BottomCurve renders correctly", () => {
-  const tree = renderer.create(<BottomCurve />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import BottomCurve from "../../components/BottomCurve/index";
 import ResourcesSection from "../../components/ResourcesSection";
 import "./index.css";
 
 const Home = () => {
   return (
     <>
-      <BottomCurve />
       <ResourcesSection />
     </>
   );
