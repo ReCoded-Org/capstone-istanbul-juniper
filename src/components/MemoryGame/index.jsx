971a7e473178bcf7da4b688cd156dee819a53994
Tarkan Mansuroğlu <tarkanmansuroglu@hotmail.com>
2020-11-03 04:38:59 +0300

    Style: add card to flip

--- src/components/MemoryGame/index.jsx
+++ src/components/MemoryGame/index.jsx
@@ -1,40 +1,44 @@
-import React, { useState } from 'react';
-import ReactCardFlip from 'react-card-flip';
-import { Card } from 'antd';
+import React, { useState } from "react";
+import ReactCardFlip from "react-card-flip";
+import { Image, Card, Row, Col } from "antd";
+import questionMark from "../../images/questionMark.png";
+import "./index.css";
 
 const MemoryGame = () => {
-    const [isFlipped, setIsFlipped] = useState(false);
+  const [isFlipped, setIsFlipped] = useState(false);
 
-    const handleClick = () => {
-        setIsFlipped(!isFlipped);
-    };
+  const handleClick = () => {
+    setIsFlipped(!isFlipped);
+  };
 
-    return (
-        <ReactCardFlip isFlipped={isFlipped} flipDirection={'vertical'}>
-            <Card
-                hoverable
-                style={{ width: 240 }}
-                onClick={handleClick}
-                cover={
-                    <img
-                        alt='example'
-                        src='https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png'
-                    />
-                }
-            ></Card>
-            <Card
-                hoverable
-                style={{ width: 240 }}
-                cover={
-                    <img
-                        alt='example'
-                        src='https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png'
-                    />
-                }
-                onClick={handleClick}
-            ></Card>
-        </ReactCardFlip>
-    );
+  return (
+    <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
+      <Card onClick={handleClick} className="memoryCard">
+        <Row>
+          <Col>
+            <Image
+              src={questionMark}
+              preview={false}
+              alt="green question mark"
+              className="cardImage"
+            />
+          </Col>
+        </Row>
+      </Card>
+      <Card onClick={handleClick} className="memoryCard">
+        <Row>
+          <Col>
+            <Image
+              src={questionMark}
+              preview={false}
+              alt="green question mark"
+              className="cardImage"
+            />
+          </Col>
+        </Row>
+      </Card>
+    </ReactCardFlip>
+  );
 };
 
 export default MemoryGame;
