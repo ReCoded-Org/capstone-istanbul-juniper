0384fd6e3375cf6f23bbd573f11f062333774f6c
Tarkan Mansuroğlu <tarkanmansuroglu@hotmail.com>
2020-11-03 22:34:09 +0300

    Feat: display all cards correctly

--- src/components/MemoryGame/index.jsx
+++ src/components/MemoryGame/index.jsx
@@ -1,48 +1,70 @@
-import React, { useState } from 'react';
-import ReactCardFlip from 'react-card-flip';
-import { Image, Card, Row, Col } from 'antd';
-import './index.css';
-import questionMark from '../../images/memoryCardFront.png';
+import React, { useState } from "react";
+import ReactCardFlip from "react-card-flip";
+import { Image, Card, Row, Col } from "antd";
+import { useTranslation } from "react-i18next";
+import imagesArr from "../memoryCardBackImages";
+import "./index.css";
+import cardFrontImage from "../../images/memoryCardFront.png";
 
 const MemoryGame = () => {
-    const [isFlipped, setIsFlipped] = useState(false);
+  const [isFlipped, setIsFlipped] = useState(false);
+  const [t] = useTranslation();
 
-    const handleClick = () => {
-        setIsFlipped(!isFlipped);
-    };
+  const imageDescriptionsArr = [
+    ...t("memoryGame.descriptions", {
+      returnObjects: true,
+    }),
+  ];
 
-    const memoryCards = backSideImages.map(image => console.log(image));
-    //     <>
-    //         <ReactCardFlip isFlipped={isFlipped} flipDirection='vertical'>
-    //             <Card onClick={handleClick} className='memoryCard'>
-    //                 <Row>
-    //                     <Col>
-    //                         <Image
-    //                             src={questionMark}
-    //                             preview={false}
-    //                             alt='green question mark'
-    //                             className='cardImage'
-    //                         />
-    //                     </Col>
-    //                 </Row>
-    //             </Card>
-    //             <Card onClick={handleClick} className='memoryCard'>
-    //                 <Row>
-    //                     <Col>
-    //                         <Image
-    //                             src={image}
-    //                             preview={false}
-    //                             alt='memory card image'
-    //                             className='cardImage'
-    //                         />
-    //                     </Col>
-    //                 </Row>
-    //             </Card>
-    //         </ReactCardFlip>
-    //     </>
-    // ));
+  // imagesArr[0]'s descriptions is stored in imageDescriptionsArr[0].
+  // same thing is repeated for all elements off arrays.
+  const memoryCardBackImages = imagesArr.map((image) => {
+    const imageIndex = imagesArr.indexOf(image);
+    const imageDescription = imageDescriptionsArr[imageIndex].description;
+    return (
+      <div key={imageDescription}>
+        <Image
+          src={image}
+          alt={imageDescription}
+          preview={false}
+          className="cardImage"
+        />
+        <figcaption className="cardFigcaption">{imageDescription}</figcaption>
+      </div>
+    );
+  });
 
-    return { memoryCards };
+  const handleClick = () => {
+    setIsFlipped(!isFlipped);
+  };
+
+  const memoryCards = memoryCardBackImages.map((cardBackImage, index) => {
+    return (
+      <div key={index}>
+        <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
+          <Card onClick={handleClick} className="memoryCard">
+            <Row>
+              <Col>
+                <Image
+                  src={cardFrontImage}
+                  preview={false}
+                  alt="green question mark"
+                  className="cardImage"
+                />
+              </Col>
+            </Row>
+          </Card>
+          <Card onClick={handleClick} className="memoryCard">
+            <Row>
+              <Col>{cardBackImage}</Col>
+            </Row>
+          </Card>
+        </ReactCardFlip>
+      </div>
+    );
+  });
+
+  return <>{memoryCards}</>;
 };
 
 export default MemoryGame;
