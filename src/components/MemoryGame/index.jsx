e37caa934d75cfd471503fbd0bfa7e5cf3695e5c
Tarkan Mansuroğlu <tarkanmansuroglu@hotmail.com>
2020-11-03 21:34:16 +0300

    add images without text

--- src/components/MemoryGame/index.jsx
+++ src/components/MemoryGame/index.jsx
@@ -1,44 +1,48 @@
-import React, { useState } from "react";
-import ReactCardFlip from "react-card-flip";
-import { Image, Card, Row, Col } from "antd";
-import questionMark from "../../images/memoryCardFront.png";
-import "./index.css";
+import React, { useState } from 'react';
+import ReactCardFlip from 'react-card-flip';
+import { Image, Card, Row, Col } from 'antd';
+import './index.css';
+import questionMark from '../../images/memoryCardFront.png';
 
 const MemoryGame = () => {
-  const [isFlipped, setIsFlipped] = useState(false);
+    const [isFlipped, setIsFlipped] = useState(false);
 
-  const handleClick = () => {
-    setIsFlipped(!isFlipped);
-  };
+    const handleClick = () => {
+        setIsFlipped(!isFlipped);
+    };
 
-  return (
-    <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
-      <Card onClick={handleClick} className="memoryCard">
-        <Row>
-          <Col>
-            <Image
-              src={questionMark}
-              preview={false}
-              alt="green question mark"
-              className="cardImage"
-            />
-          </Col>
-        </Row>
-      </Card>
-      <Card onClick={handleClick} className="memoryCard">
-        <Row>
-          <Col>
-            <Image
-              src={questionMark}
-              preview={false}
-              alt="green question mark"
-              className="cardImage"
-            />
-          </Col>
-        </Row>
-      </Card>
-    </ReactCardFlip>
-  );
+    const memoryCards = backSideImages.map(image => console.log(image));
+    //     <>
+    //         <ReactCardFlip isFlipped={isFlipped} flipDirection='vertical'>
+    //             <Card onClick={handleClick} className='memoryCard'>
+    //                 <Row>
+    //                     <Col>
+    //                         <Image
+    //                             src={questionMark}
+    //                             preview={false}
+    //                             alt='green question mark'
+    //                             className='cardImage'
+    //                         />
+    //                     </Col>
+    //                 </Row>
+    //             </Card>
+    //             <Card onClick={handleClick} className='memoryCard'>
+    //                 <Row>
+    //                     <Col>
+    //                         <Image
+    //                             src={image}
+    //                             preview={false}
+    //                             alt='memory card image'
+    //                             className='cardImage'
+    //                         />
+    //                     </Col>
+    //                 </Row>
+    //             </Card>
+    //         </ReactCardFlip>
+    //     </>
+    // ));
+
+    return { memoryCards };
 };
 
 export default MemoryGame;
---
+++ src/components/memoryCardBackImages.js
@@ -0,0 +1,32 @@
+import acidRain from '../images/memoryCardBack/acidRain.svg';
+import airPollution from '../images/memoryCardBack/airPollution.svg';
+import alternateEnergy from '../images/memoryCardBack/alternateEnergy.svg';
+import chemicalWaste from '../images/memoryCardBack/chemicalWaste.svg';
+import deforestation from '../images/memoryCardBack/deforestation.svg';
+import globalWarming from '../images/memoryCardBack/globalWarming.svg';
+import greenhouseEffect from '../images/memoryCardBack/greenhouseEffect.svg';
+import littering from '../images/memoryCardBack/littering.svg';
+import meltingIceCaps from '../images/memoryCardBack/meltingIceCaps.svg';
+import ozoneHole from '../images/memoryCardBack/ozoneHole.svg';
+import recycle from '../images/memoryCardBack/recycle.svg';
+import waterPollution from '../images/memoryCardBack/waterPollution.svg';
+
+const memoryCardBackImages = () => {
+    const images = [
+        acidRain,
+        airPollution,
+        alternateEnergy,
+        chemicalWaste,
+        deforestation,
+        globalWarming,
+        greenhouseEffect,
+        littering,
+        meltingIceCaps,
+        ozoneHole,
+        recycle,
+        waterPollution,
+    ];
+    return images;
+};
+
+export default memoryCardBackImages;
