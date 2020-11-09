f7f0c25511587f783760c2307e1ee6cb4009bf43
Tarkan Mansuroğlu <tarkanmansuroglu@hotmail.com>
2020-11-10 00:19:20 +0300

    game component and functions moved under 'memorygame' folder

---
+++ src/components/memorygame/memoryCardBackSideImages.jsx
@@ -0,0 +1,29 @@
+import acidRain from '../../images/memoryCardBack/acidRain.svg';
+import airPollution from '../../images/memoryCardBack/airPollution.svg';
+import alternateEnergy from '../../images/memoryCardBack/alternateEnergy.svg';
+import chemicalWaste from '../../images/memoryCardBack/chemicalWaste.svg';
+import deforestation from '../../images/memoryCardBack/deforestation.svg';
+import globalWarming from '../../images/memoryCardBack/globalWarming.svg';
+import greenhouseEffect from '../../images/memoryCardBack/greenhouseEffect.svg';
+import littering from '../../images/memoryCardBack/littering.svg';
+import meltingIceCaps from '../../images/memoryCardBack/meltingIceCaps.svg';
+import ozoneHole from '../../images/memoryCardBack/ozoneHole.svg';
+import recycle from '../../images/memoryCardBack/recycle.svg';
+import waterPollution from '../../images/memoryCardBack/waterPollution.svg';
+
+const CARD_BACKSIDE_IMAGES = [
+    acidRain,
+    airPollution,
+    alternateEnergy,
+    chemicalWaste,
+    deforestation,
+    globalWarming,
+    greenhouseEffect,
+    littering,
+    meltingIceCaps,
+    ozoneHole,
+    recycle,
+    waterPollution,
+];
+
+export default CARD_BACKSIDE_IMAGES;
