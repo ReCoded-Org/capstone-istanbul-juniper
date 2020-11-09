f30797256a60f9beab56b8bc30a09a96fe0e5165
Sule Sinikoglu <ssinikoglu@gmail.com>
2020-11-09 16:07:08 +0300

    Build: add matching game card component

---
+++ src/components/MatchingGameCard/index.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+// Matching card has two types. One of them is word card, other one is image card.
+// Single Matching Game Card which is used for word cards and image cards.
+const MatchingGameCard = (props) => {
+  const dragStart = (e) => {
+    const target = e.target;
+    e.dataTransfer.setData("cardId", target.id);
+    setTimeout(() => {
+      target.style.display = "none";
+    }, 0);
+  };
+  const dragOver = (e) => {
+    e.stopPropagation();
+  };
+  return (
+    <div
+      id={props.id}
+      className={props.className}
+      draggable={props.draggable}
+      onDragStart={dragStart}
+      onDragOver={dragOver}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+export default MatchingGameCard;
