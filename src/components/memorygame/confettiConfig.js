c926a75ef23276b2a2fe2b1b5f8d502b6f4e03b1
Tarkan Mansuroğlu <tarkanmansuroglu@hotmail.com>
2020-11-12 04:14:25 +0300

    files transferred

---
+++ src/components/memorygame/confettiConfig.js
@@ -0,0 +1,15 @@
+const confettiConfig = {
+  angle: "180",
+  spread: "360",
+  startVelocity: 40,
+  elementCount: "200",
+  dragFriction: 0.12,
+  duration: "4100",
+  stagger: "2",
+  width: "10px",
+  height: "10px",
+  perspective: "500px",
+  colors: ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"],
+};
+
+export default confettiConfig;
