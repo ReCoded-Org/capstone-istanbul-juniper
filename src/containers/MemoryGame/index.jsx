12eb30226a8dd1e1cde4603aea0bc4a49a8f88d7
Tarkan Mansuroğlu <tarkanmansuroglu@hotmail.com>
2020-11-15 08:02:31 +0300

    Refactor: refactor code to use container

--- src/containers/MemoryGame/index.jsx
+++ src/containers/MemoryGame/index.jsx
@@ -1,13 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
-import MemoryGame from "../../components/memorygame/MemoryGame/index";
+import { Col, Row } from "antd";
+import { useTranslation } from "react-i18next";
+// Check https://github.com/daniel-lundin/react-dom-confetti for 'react-dom-confetti'
+import Confetti from "react-dom-confetti";
+import confettiConfig from "../../components/memorygame/confettiConfig";
+import MemoryGameFactList from "../../components/memorygame/MemoryGameFactList";
+import MemoryGameBoard from "../../components/memorygame/MemoryGameBoard";
 
-const MemoryGameContainer = () => {
+const MemoryGame = () => {
+  const [t] = useTranslation();
+  // from line 13 to 17 all const are string translations
+  const factsTitle = t("memoryGame.facts");
+  const emptyFactListMessage = t("memoryGame.emptyFactListMessage");
+  const gameTitle = t("memoryGame.title");
+  const gameHeader = t("memoryGame.header");
+  const [isGameCompleted, setIsGameCompleted] = useState(false);
+  // matchedCards is being used to display environmental facts.
+  // It is an array of object(states).Each object is a successfully matched card state.
+  // When a match is made only state of first card gets pushed inside matchedCards.
+  // Because matched cards have same state. Otherwise same fact would be displayed twice.
+  const [matchedCards, setMatchedCards] = useState([]);
+
   return (
-    <div className="memoryGameContainer">
-      <MemoryGame />
-    </div>
+    <Row justify="center" className="memoryGameContainer">
+      {/* Confetti displays confetti effect to congratulate user when game is completed */}
+      <Confetti active={isGameCompleted} config={confettiConfig} />
+      <Col xs={22} md={20} xl={17}>
+        <h1 className="memoryGameContainer__title">{gameTitle}</h1>
+        <h3 className="memoryGameContainer__description">{gameHeader}</h3>
+        <Row justify="center">
+          <MemoryGameBoard
+            setMatchedCards={setMatchedCards}
+            matchedCards={matchedCards}
+            setIsGameCompleted={setIsGameCompleted}
+          />
+        </Row>
+      </Col>
+      <Col xs={20}>
+        <Confetti active={isGameCompleted} config={confettiConfig} />
+        <MemoryGameFactList
+          facts={matchedCards}
+          title={factsTitle}
+          emptyFactListMessage={emptyFactListMessage}
+        />
+      </Col>
+    </Row>
   );
 };
 
-export default MemoryGameContainer;
+export default MemoryGame;
