20d425bb716322253147f293e35e6d0dbb08ea9b
Tarkan Mansuroğlu <67609059+tmansuroglu@users.noreply.github.com>
2020-11-16 21:18:02 +0300

    memory game fact list refactored (#56)
    * first commit
    
    * fix commented parts
    
    * fix comments

--- src/components/memorygame/MemoryGameFactList/index.jsx
+++ src/components/memorygame/MemoryGameFactList/index.jsx
@@ -1,31 +1,49 @@
 import React from "react";
 import "./index.css";
 import { List, Card } from "antd";
+import { useTranslation } from "react-i18next";
 
 /**
- * Upon matching cards, a fact related to matched cards appear on screen
+ * Upon matching cards, a fact related to matched cards appears on screen
  * This component creates the list that will hold all facts
- * "facts" is an array of objects
- * "title" is a string that is translated by i18n. It is used to describe what the list holds.
- * If the language is English "title" will be "Facts"
+ * "facts" is an array of state objects
+ * To see state object example check src/components/memorygame/MemoryGameBoard/index.jsx
  */
-const MemoryGameFactList = ({ facts, title }) => {
-  return (
-    <>
-      <h1 className="memoryGame__title">{title}</h1>
+const MemoryGameFactList = ({ facts }) => {
+  const [t] = useTranslation();
+  let factListContent;
+  // facts is an empty array by default, it gets filled when user makes successful match
+  if (facts.length !== 0) {
+    factListContent = (
       <List
+        className="memoryGameFactListContainer__list"
         dataSource={facts}
         renderItem={(fact) => (
-          <List.Item>
-            <Card className="memoryGame__card">
-              <a target="__blank" href={fact.link}>
+          <List.Item className="memoryGameFactListContainer__list__item">
+            <a target="__blank" href={fact.link}>
+              <Card className="memoryGameFactListContainer__list__item__card">
                 {fact.phrase}
-              </a>
-            </Card>
+              </Card>
+            </a>
           </List.Item>
         )}
       />
-    </>
+    );
+  } else {
+    factListContent = (
+      <h5 className="memoryGameFactListContainer__noFacts">
+        {t("memoryGame.emptyFactListMessage")}
+      </h5>
+    );
+  }
+
+  return (
+    <div className="memoryGameFactListContainer" id="memoryGameFactList">
+      <h1 className="memoryGameFactListContainer__title">
+        {t("memoryGame.facts")}
+      </h1>
+      {factListContent}
+    </div>
   );
 };
 
