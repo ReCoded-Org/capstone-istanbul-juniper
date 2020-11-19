39021193ee77fabf83928ccf6a5e1b61f488ba29
ibrahim aljarad <ibrahimealkalil@gmail.com>
2020-11-20 02:11:09 +0300

    profilepage container added

---
+++ src/containers/ProfilePageContainer/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProfilePage from "./../../components/ProfilePage/index";
+
+const ProfilePageContainer = () => {
+  return (
+    <>
+    <ProfilePage />
+    </>
+  );
+};
+
+export default ProfilePageContainer;
