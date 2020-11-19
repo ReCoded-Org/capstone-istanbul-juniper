415ed2f3bda6ca8f0d06942bfc3722d3388f9b9e
Tarkan Mansuroğlu <67609059+tmansuroglu@users.noreply.github.com>
2020-11-19 16:57:47 +0300

    Games page progress section (#65)
    * container and component created
    
    * UI created
    
    * Style: made  background color lighter
    
    * Feat: add translations
    
    * fix translations
    
    * set variables
    
    * Feat: add more translation
    
    * improve translations
    
    * add test
    
    * general fixes reverted
    
    * fix translations
    
    * delete default parameters
    
    * Fix: level translation
    
    * implement requested changes
    
    * fix comments and change props
    
    * modify translation key
    
    Co-authored-by: Ammar Sammour <35445761+Ammar-64@users.noreply.github.com>

--- src/containers/GamesPage/index.jsx
+++ src/containers/GamesPage/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import ProgressSection from "../../components/gamespage/GamesPageProgressSection";
 import GamesPageImageGrid from "../../components/gamespage/GamesPageImageGrid";
 import BottomCurve from "../../components/BottomCurve";
 
 const GamesPage = () => {
   return (
     <>
+      <ProgressSection />
       <BottomCurve />
       <GamesPageImageGrid />
     </>
