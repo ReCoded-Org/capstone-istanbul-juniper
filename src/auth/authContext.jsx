86bdc80c64b1e7af089f8c03d2e23c586eee8bf8
YamanSammani <67820326+YamanSammani@users.noreply.github.com>
2020-11-20 09:25:47 +0000

    firebase auth - updated (#79)
    Co-authored-by: Ammar Sammour <35445761+Ammar-64@users.noreply.github.com>

---
+++ src/auth/authContext.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import firestore, { auth } from "../firebaseConfig";
+
+export const AuthContext = React.createContext({
+  user: null,
+  setUser: null,
+});
+
+const AuthProvider = (props) => {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((auth) => {
+      processAuth(auth, setUser);
+    });
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export const processAuth = async (auth, setUser) => {
+  if (!auth) {
+    return setUser(null);
+  }
+  // A unique user ID, assigned to the requesting user, we're calling/maticing this ID from our server.
+  let firestoreResult = await firestore.doc("users/" + auth.uid).get();
+  if (!firestoreResult.data()) {
+    await firestore.collection("users").doc(auth.uid).set({
+      uid: auth.uid,
+      fullname: auth.displayName,
+      age: null,
+      userExperiencePoints: 0,
+    });
+    firestoreResult = await firestore.doc("users/" + auth.uid).get();
+  }
+  const user = {
+    isLoggedin: true,
+    uid: auth.uid,
+    email: auth.email,
+    fullname: firestoreResult.data().fullname ?? auth.displayName,
+    age: firestoreResult.data().age ?? null,
+    userExperiencePoints: firestoreResult.data().userExperiencePoints ?? 0,
+  };
+  await setUser(user);
+};
+
+export default AuthProvider;
