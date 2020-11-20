a09cfa3d6116117e5e30e6f51e9d9bff51df8930
Tarkan Mansuroğlu <tarkanmansuroglu@hotmail.com>
2020-11-21 02:13:52 +0300

    add source to regex

--- src/components/register/registerHelpers.js
+++ src/components/register/registerHelpers.js
@@ -1,4 +1,5 @@
 export function validateEmail(email) {
+  // regex source https://emailregex.com/
   const re = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   return re.test(email);
 }
