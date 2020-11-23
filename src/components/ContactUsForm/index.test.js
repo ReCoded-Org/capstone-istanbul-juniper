eae6a656df4c15658e96a47f793adda8c8701f0e
Sule Sinikoglu <62522087+ssinikoglu@users.noreply.github.com>
2020-11-23 11:56:19 +0300

    Fix contact form responsiveness (#123)
    * Style: fix responsive settings
    
    * Style: update texarea height
    
    * Test: update tests after changes
    
    * Test: update tests
    
    * fix test
    
    Co-authored-by: YamanSammani <yaman.mys@gmail.com>
    Co-authored-by: Ammar Sammour <35445761+Ammar-64@users.noreply.github.com>

--- src/components/ContactUsForm/index.test.js
+++ src/components/ContactUsForm/index.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import ContactUsForm from "./index.jsx";
 import renderer from "react-test-renderer";
 
-it("ContactUsForm snapshot test", () => {
+it("ContactUsForm snapshot test ", () => {
   const tree = renderer.create(<ContactUsForm />).toJSON();
   expect(tree).toMatchSnapshot();
 });
