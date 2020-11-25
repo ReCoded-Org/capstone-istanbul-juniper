2025366563a4cadf5f766e62aacddbb7fb8564c7
YamanSammani <67820326+YamanSammani@users.noreply.github.com>
2020-11-25 01:20:41 +0000

    fix resource section router (#140)
    * fix resource section router
    
    * fix test
    
    * fix test
    
    Co-authored-by: Ammar Sammour <35445761+Ammar-64@users.noreply.github.com>

--- src/components/ResourcesSection/ResourcesSection.test.jsx
+++ src/components/ResourcesSection/ResourcesSection.test.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import ResourcesSection from ".";
+import { BrowserRouter as Router } from "react-router-dom";
 
 it("ResourcesSection renders correctly", () => {
-  const tree = renderer.create(<ResourcesSection />).toJSON();
+  const tree = renderer
+    .create(
+      <Router>
+        <ResourcesSection />
+      </Router>
+    )
+    .toJSON();
   expect(tree).toMatchSnapshot();
 });
--- src/components/ResourcesSection/index.jsx
+++ src/components/ResourcesSection/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Col, Row } from "antd";
 import "./index.css";
 import "antd/dist/antd.css";
-import { Link, BrowserRouter as Router } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import recycleImg from "./../../images/recycleResourceSetion.svg";
 import earthImg from "./../../images/earthResourceSetion.svg";
@@ -12,58 +12,56 @@ const ResourcesSection = () => {
   const [t] = useTranslation();
   return (
     <div className="resourceCardWrapper">
-      <Router>
-        <Row justify="center" gutter={[16, 16]}>
-          <Col>
-            <Row justify="center">
-              <Col className="resourceCardWrapper__cardName">
-                <Link to="/pollution">
-                  {t("home.resourcesSection.pollution")}
-                </Link>
-              </Col>
-            </Row>
-            <Row justify="center">
-              <Col className="resourceCardWrapper__cardImage">
-                <Link to="/pollution">
-                  <img alt="pollution" src={pollutionImg} />
-                </Link>
-              </Col>
-            </Row>
-          </Col>
-          <Col>
-            <Row justify="center">
-              <Col className="resourceCardWrapper__cardName">
-                <Link to="/climatechange">
-                  {t("home.resourcesSection.climateChange")}
-                </Link>
-              </Col>
-            </Row>
-            <Row justify="center">
-              <Col className="resourceCardWrapper__cardImage">
-                <Link to="/climatechange">
-                  <img alt="Climate Change" src={earthImg} />
-                </Link>
-              </Col>
-            </Row>
-          </Col>
-          <Col>
-            <Row justify="center">
-              <Col className="resourceCardWrapper__cardName">
-                <Link to="/recycle">
-                  {t("home.resourcesSection.reduceReuseRecycle")}
-                </Link>
-              </Col>
-            </Row>
-            <Row justify="center">
-              <Col className="resourceCardWrapper__cardImage">
-                <Link to="/recycle">
-                  <img alt="Recycle" src={recycleImg} />
-                </Link>
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-      </Router>
+      <Row justify="center" gutter={[16, 16]}>
+        <Col>
+          <Row justify="center">
+            <Col className="resourceCardWrapper__cardName">
+              <Link to="/resources">
+                {t("home.resourcesSection.pollution")}
+              </Link>
+            </Col>
+          </Row>
+          <Row justify="center">
+            <Col className="resourceCardWrapper__cardImage">
+              <Link to="/resources">
+                <img alt="pollution" src={pollutionImg} />
+              </Link>
+            </Col>
+          </Row>
+        </Col>
+        <Col>
+          <Row justify="center">
+            <Col className="resourceCardWrapper__cardName">
+              <Link to="/resources">
+                {t("home.resourcesSection.climateChange")}
+              </Link>
+            </Col>
+          </Row>
+          <Row justify="center">
+            <Col className="resourceCardWrapper__cardImage">
+              <Link to="/resources">
+                <img alt="Climate Change" src={earthImg} />
+              </Link>
+            </Col>
+          </Row>
+        </Col>
+        <Col>
+          <Row justify="center">
+            <Col className="resourceCardWrapper__cardName">
+              <Link to="/resources">
+                {t("home.resourcesSection.reduceReuseRecycle")}
+              </Link>
+            </Col>
+          </Row>
+          <Row justify="center">
+            <Col className="resourceCardWrapper__cardImage">
+              <Link to="/resources">
+                <img alt="Recycle" src={recycleImg} />
+              </Link>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
     </div>
   );
 };
